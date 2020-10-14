97d07b2b0bd8444f5019bd2e09ed00fb542e9168
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-10-14 16:16:48 +0100

    build: create main page

---
+++ lib/screens/main_page.dart
@@ -0,0 +1,28 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  static const String id = "mainpage";
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Main Page'),
+      ),
+      body: SafeArea(
+        child: MaterialButton(
+          onPressed: () {},
+          color: Colors.blue,
+          minWidth: 200,
+          child: Text('Hello'),
+        ),
+      ),
+    );
+  }
+}
